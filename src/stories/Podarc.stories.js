39fb1810818dea262bf86012983239e37aa17e99
Jalal999 <jmammadli2021@ada.edu.az>
2024-04-09 12:42:42 +0200

    Add StoryBook dependency and node_module files

---
+++ src/stories/Podarc.stories.js
@@ -0,0 +1,10 @@
+import TestPodarc from '../TestPodarc';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+export default {
+  title: 'Example/Podarc',
+  component: TestPodarc,
+};
+
+export const Podarc = {};
+
